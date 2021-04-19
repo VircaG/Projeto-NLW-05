0314af34d7036989e7a6e3ba8b699bf0e7caec96
VircaG <vircagoncalves39@gmail.com>
2021-04-19 20:18:51 +0100

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import express, { request, response } from "express";
+
+
+const app = express();
+
+/**
+ * GET = Buscas
+ * POST = Criação
+ * PUT = Alteração
+ * DELETE = Deletar
+ * PATCH = Alterar informação específica
+*/
+
+app.get("/", (request, response) => {
+    return response.json({
+        message: "Olá NLW 05!"
+    })
+});
+
+app.post("/user", (request, response) => {
+    return response.json({ message: "Utilizador Salvo com sucesso!" });
+
+});
+
+
+
+app.listen(3333, () => console.log("Server is running on port 3333"));
